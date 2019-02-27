132ca190e4a2cf4a0d51cef4fa068d5b48a85cca
Contador <ramboowang@163.com>
2019-02-27 11:33:04 +0800

    第一次新建实体类

---
+++ src/domain.java
@@ -0,0 +1,9 @@
+public class domain {
+    private String username;
+    private Integer password;
+
+    public domain(String username, Integer password) {
+        this.username = username;
+        this.password = password;
+    }
+}
