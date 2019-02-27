15b610479b061753c9563d5097500b9d97228ed5
Contador <ramboowang@163.com>
2019-02-27 11:34:42 +0800

    第二次修改

--- src/domain.java
+++ src/domain.java
@@ -2,6 +2,22 @@ public class domain {
     private String username;
     private Integer password;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getPassword() {
+        return password;
+    }
+
+    public void setPassword(Integer password) {
+        this.password = password;
+    }
+
     public domain(String username, Integer password) {
         this.username = username;
         this.password = password;
